8c71a7f374df62dec8f8e3a0a50c77f7c72fd96a
juancarlozamora <32862644+juancarlozamora@users.noreply.github.com>
2018-03-18 16:30:25 +0800

    Question 6
    Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen greater changes over time in motor vehicle emissions?

---
+++ plot6.R
@@ -0,0 +1,29 @@
+# Question 6
+
+require(dplyr)
+baltcitymary.emissions<-summarise(group_by(filter(NEI, fips == "24510"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
+losangelscal.emissions<-summarise(group_by(filter(NEI, fips == "06037"& type == 'ON-ROAD'), year), Emissions=sum(Emissions))
+
+baltcitymary.emissions$County <- "Baltimore City, MD"
+losangelscal.emissions$County <- "Los Angeles County, CA"
+both.emissions <- rbind(baltcitymary.emissions, losangelscal.emissions)
+
+require(ggplot2)
+ggplot(both.emissions, aes(x=factor(year), y=Emissions, fill=County,label = round(Emissions,2))) +
+  geom_bar(stat="identity") + 
+  facet_grid(County~., scales="free") +
+  ylab(expression("total PM"[2.5]*" emissions in tons")) + 
+  xlab("year") +
+  ggtitle(expression("Motor vehicle emission variation in Baltimore and Los Angeles in tons"))+
+  geom_label(aes(fill = County),colour = "white", fontface = "bold")
+
+
+
+# Save the plot as a png file
+
+plot6 <- function() {
+  dev.copy(png, file="plot6.png", width=480, height=480)
+  dev.off()
+  cat("Plot6.png has been saved in", getwd())
+}
+plot6()
